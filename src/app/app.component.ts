88d05ac5b49042edbbda21a032f6d50109ad96ed
Paul Ramon <peramon98.18@gmail.com>
2023-08-15 14:40:07 -0500

    Using directives ng if and ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'challengeOne';
+  url = 'https://miro.medium.com/v2/resize:fit:563/1*wmiHccq6imyMUakUZV_f6g.png';
+  
+  // Two-way Data Binding
+  name!: string;
+
+  // Directives
+  cities = ['Madrid','Barcelona','Sevilla']
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+// Es el modulo principalmente de la aplicacion
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    // Se inyectan otros modulos
   ],
   providers: [],
   bootstrap: [AppComponent]
